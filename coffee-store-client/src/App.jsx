cdd16fb1659df1c91442c10ae1c449c36b04a323
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2025-01-16 17:20:54 +0600

    change a title name

--- coffee-store-client/src/App.jsx
+++ coffee-store-client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-6xl text-purple-600'>Vite + React</h1>
+      <h1 className='text-6xl text-purple-600'>Coffee service</h1>
     </>
   )
 }
